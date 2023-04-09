2569ef7b7f16c13a68a1bf2960e40dc630cc0ad2
achchaimae <achchaimae1@gmail.com>
2023-04-09 02:15:37 +0000

    tag comp 📌

---
+++ src/components/Jobs/Tags.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mt-8 flex justify-center">
+                    <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center">
+                       UX/UI
+                    </button>
+                    <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2  leading-none flex items-center">
+                        Web Developer
+                    </button>
+                
+                    <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2  leading-none flex items-center">
+                       Data Science
+                    </button>
+                
+                    <!-- <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2   leading-none flex items-center">
+                        Software Engineer
+                    </button>
+                
+                    <button type="button" class="rounded-full px-4  bg-[#9191E9] w-24  justify-center leading-none flex items-center">
+                        Data Science
+                    </button> -->
+                </div>
+</template>
+<script setup>
+</script>
