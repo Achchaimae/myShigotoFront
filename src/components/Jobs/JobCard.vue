0a7efcf70a556629605f5857554b975042125bf6
achchaimae <achchaimae1@gmail.com>
2023-04-09 02:15:16 +0000

    job cards 🃏

---
+++ src/components/Jobs/JobCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="bg-white w-64 h-72 text-black rounded-lg mt-4">
+            <div class="flex">
+                <div class="relative">
+                    <img :src="CompanyLogo" class="absolute top-8 left-3 w-16" alt />
+                    <img src="@/assets/images/card.png" alt />
+                </div>
+                <div class="p-4">
+                    <h1 class="font-bold text-xl">{{ CompanyName }}</h1>
+                    <p>1 hour ago</p>
+                </div>
+            </div>
+
+            <div class="p-4">
+                <h1 class="text-xl font-bold">{{ OffreName }}</h1>
+                <p class="font-regular text-gray-400 font-['Display_Fair']">{{ OffreDesciption }}</p>
+            </div>
+            <div class="flex justify-end px-4">
+                <button
+                    class="border-2 rounded-full px-8 py-2 mb-4 border-[#531CB3] hover:bg-[#531CB3] hover:text-white hover:border-white hover:cursor-pointer">Apply</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+    CompanyLogo: {
+        type: String,
+        required: true
+    },
+    CompanyName: {
+        type: String,
+        required: true
+    },
+    OffreName: {
+        type: String,
+        required: true
+    },
+    OffreDesciption: {
+        type: String,
+        required: true
+    }
+});
+</script>
---
+++ src/components/Jobs/PostCards.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <div class="flex justify-around gap-4 flex-wrap">
+            <JobCard v-for="card in obj.cards" :CompanyName="card.CompanyName" :CompanyLogo="card.CompanyLogo" :OffreName="card.OffreName" :OffreDesciption="card.OffreDesciption" />
+        </div>
+    </main>
+
+</template>
+<script setup>
+import JobCard from '@/components/Home/JobCard.vue'
+import { reactive } from 'vue'
+
+const obj = reactive({
+    cards : [
+        {CompanyName: 'Oracle', CompanyLogo: 'src/assets/images/Oracle.png', OffreName: 'Web Developer', OffreDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+        {CompanyName: 'Google', CompanyLogo: 'src/assets/images/Google.png', OffreName: 'UX/UI', OffreDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+        {CompanyName: 'Meta', CompanyLogo: 'src/assets/images/Meta.png', OffreName: 'Big Data', OffreDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+        {CompanyName: 'Microsoft', CompanyLogo: 'src/assets/images/Microsoft.png', OffreName: 'Data Science', OffreDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+        {CompanyName: 'Meta', CompanyLogo: 'src/assets/images/Meta.png', OffreName: 'Backend Engineer', OffreDesciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+        {CompanyName: 'Oracle', CompanyLogo: 'src/assets/images/Oracle.png', OffreName: 'Software Engineer', OffreDesciption: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'},
+    ],
+});
+
+
+</script>
