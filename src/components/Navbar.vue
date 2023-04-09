0730d6c6b7ec4bb7fc4b7d107454d9172a3f0541
achchaimae <achchaimae1@gmail.com>
2023-04-09 01:07:27 +0000

    responsive navbar 👌

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,16 +3,43 @@
     <div class="w-36">
       <img src="@/assets/images/logo.png" alt="logo" />
     </div>
-    <div class="flex justify-between gap-8 font-semibold mt-1">
+    <div class="hidden md:flex justify-between gap-8 font-semibold mt-1">
       <a href="/"> Home </a>
       <a href="/Jobs"> Jobs </a>
       <a href="/company"> Companies </a>
       <a href="#"> Messages </a>
     </div>
-    <div class="bg-[#9191E9;] p-2 rounded">
+    <div class="flex md:hidden">
+      <button @click="toggleMenu" class="flex items-center px-3 py-2 rounded text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          <path v-if="!menuOpen" fill-rule="evenodd" clip-rule="evenodd" d="M3 5h18v2H3V5zm0 6h18v2H3v-2zm0 6h18v2H3v-2z"></path>
+          <path v-else fill-rule="evenodd" clip-rule="evenodd" d="M3 7h18v2H3V7zm0 6h18v2H3v-2zm0 6h18v2H3v-2z"></path>
+        </svg>
+      </button>
+      <div v-if="menuOpen" class="absolute right-0 top-12 w-48 py-2 bg-gray-800 rounded shadow-xl z-10">
+        <a href="/" class="block px-4 py-2 text-white hover:bg-gray-700"> Home </a>
+        <a href="/Jobs" class="block px-4 py-2 text-white hover:bg-gray-700"> Jobs </a>
+        <a href="/company" class="block px-4 py-2 text-white hover:bg-gray-700"> Companies </a>
+        <a href="#" class="block px-4 py-2 text-white hover:bg-gray-700"> Messages </a>
+      </div>
+    </div>
+    <div class="hidden md:block bg-[#9191E9;] p-2 rounded">
       <a href="/Register">Sing up</a>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    }
+  }
+}
+</script>
