729a810e6d8621380b270e40e42dd7a04dbe6b31
achchaimae <achchaimae1@gmail.com>
2023-04-09 02:16:07 +0000

    messages page📧

---
+++ src/views/user/Messages.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+       <Navbar />
+       <section >
+   
+       </section>
+       <Footer/>
+    </main>
+</template>
+<script setup>
+// //import the  components
+import Navbar from "@/components/Navbar.vue";
+
+import Footer from "@/components/Footer.vue";
+</script>
