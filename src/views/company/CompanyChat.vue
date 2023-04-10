3aecbff185bd8364c1b7c9bd886c254f4e07dee0
achchaimae <achchaimae1@gmail.com>
2023-04-10 16:05:10 +0000

    company chat

---
+++ src/views/company/CompanyChat.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="flex text-white">
+        <Navbar/>
+        <section class="w-[80%] bg-white m-auto h-[90vh] rounded-lg p-4">
+         
+        </section>
+    </main>
+</template>
+<script setup>
+import Navbar from '@/components/Companies/Navbar.vue'
+</script>
