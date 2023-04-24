22e81fa2cc787686be5ec41a113b77731818aead
achchaimae <achchaimae1@gmail.com>
2023-04-24 18:45:39 +0100

    the tags is done from the back but need to show up in the front

--- src/components/Jobs/Tags.vue
+++ src/components/Jobs/Tags.vue
@@ -1,24 +1,33 @@
 <template>
     <div class="mt-8 flex justify-center">
-                    <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center">
-                       UX/UI
-                    </button>
-                    <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2  leading-none flex items-center">
-                        Web Developer
-                    </button>
-                
-                    <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2  leading-none flex items-center">
-                       Data Science
-                    </button>
-                
-                    <!-- <button type="button" class="rounded-full px-4 mr-2  bg-[#9191E9] w-24  justify-center text-white p-2   leading-none flex items-center">
-                        Software Engineer
-                    </button>
-                
-                    <button type="button" class="rounded-full px-4  bg-[#9191E9] w-24  justify-center leading-none flex items-center">
-                        Data Science
-                    </button> -->
-                </div>
-</template>
-<script setup>
-</script>
+      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('UI')">
+        UX/UI
+      </button>
+      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('Web Developer')">
+        Web Developer
+      </button>
+      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('Data Science')">
+        Data Science
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+    import axios from 'axios';
+  
+    const searchPosts = async (tag) => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/V1/posts/tag/${tag}`);
+        const posts = response.data;
+       const data=posts[0];
+        console.log(data);
+
+        
+        
+      } catch (error) {
+        // alert('Something went wrong')
+        console.error(error);
+      }
+    };
+  </script>
+  
