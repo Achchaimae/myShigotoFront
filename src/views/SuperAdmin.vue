32cec0849e7ea71e5cb88685661df5a67ee7fc35
achchaimae <achchaimae1@gmail.com>
2023-04-25 16:49:52 +0100

    super admin

--- src/views/SuperAdmin.vue
+++ src/views/SuperAdmin.vue
@@ -1,10 +1,9 @@
 <template>
     <main class="text-white">
-        
-        <section  class="flex text-white justify-between flex-row  m-4">
+        <section class="flex text-white justify-between flex-row  m-4">
             <div class="w-36 ">
                 <img src="@/assets/images/logo.png" alt="logo" />
-                 
+
             </div>
             <div @click="logout">
                 <i class="fa-solid fa-sign-out text-2xl"></i>
@@ -13,68 +12,69 @@
         <section class="w-[90%] bg-white m-auto h-[90vh] rounded-lg p-4 text-center flex flex-col gap-8 text-black">
             <h1 class="text-2xl font-bold">Welcome Back <span class="text-[#9191E9]">Achchaimae Khalaf</span></h1>
             <div>
-                <table  class="min-w-max w-full table-auto">
-            
-            <thead>
-                <tr  class="bg-gray-200 text-gray-600 h-14 text-sm py-4 leading-normal">
-                    <th>Company </th>
-                    <th>City </th>
-                    <th>Validation Document</th>
-                    <th>Email</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody class="text-gray-600 text-sm font-light items-center ">
-                
-                <tr class="border-b border-gray-200 hover:bg-gray-100 items-center" v-for="companies in company">
+                <table class="min-w-max w-full table-auto">
 
-                    <td class="py-3 px-6 text-left whitespace-nowrap flex ">
-                        <div class="mr-2">
-                            <img class="w-12 h-12 rounded-full  object-contain bg-red-300" :src="companies.image"/>
-                        </div>
-                        <span class="font-semibold">
-                           {{companies.FirstName}}
-                        </span>
-                    </td>
-                    <td class="py-3 px-6 text-left whitespace-nowrap">
-                        {{ companies.city}}
-                    </td>
-                    <td class="py-3 px-6 text-left whitespace-nowrap">
-                            <button class="bg-blue-500 text-white rounded-full p-2" @click="download(companies.document_validation)">
-                                Download
-                            </button>
-                    </td>
-                    <td class="py-3 px-6 text-left whitespace-nowrap">
-                        {{ companies.email}}
-                    </td>
-                    <td class="py-3 px-6 text-left whitespace-nowrap gap-2 flex ">
-                        <!-- accept and refuse button  -->
-                        <div v-if="companies.validation === 'pending'">
-                            <button class="bg-green-500 text-white rounded-lg p-2" @click="accept(companies.id)">Accept</button>
-                            <button class="bg-red-500 text-white rounded-lg p-2" @click="refuse(companies.id)">Refuse</button>
-                        </div>
-                        <div v-else>
-                            {{ companies.validation }}
-                        </div>
-                        
-                    </td>
-                </tr> 
-                
-            </tbody>
-       </table>
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 h-14 text-sm py-4 leading-normal">
+                            <th>Company </th>
+                            <th>City </th>
+                            <th>Validation Document</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light items-center ">
+
+                        <tr class="border-b border-gray-200 hover:bg-gray-100 items-center" v-for="companies in company">
+
+                            <td class="py-3 px-6 text-left whitespace-nowrap flex ">
+                                <div class="mr-2">
+                                    <img class="w-12 h-12 rounded-full  object-contain bg-red-300" :src="companies.image" />
+                                </div>
+                                <span class="font-semibold">
+                                    {{ companies.FirstName }}
+                                </span>
+                            </td>
+                            <td class="py-3 px-6 text-left whitespace-nowrap">
+                                {{ companies.city }}
+                            </td>
+                            <td class="py-3 px-6 text-left whitespace-nowrap">
+                                <button class="bg-blue-500 text-white rounded-full p-2"
+                                    @click="download(companies.document_validation)">
+                                    Download
+                                </button>
+                            </td>
+                            <td class="py-3 px-6 text-left whitespace-nowrap">
+                                {{ companies.email }}
+                            </td>
+                            <td class="py-3 px-6 text-left whitespace-nowrap gap-2 flex ">
+                                <!-- accept and refuse button  -->
+                                <div v-if="companies.validation === 'pending'">
+                                    <button class="bg-green-500 text-white rounded-lg p-2"
+                                        @click="accept(companies.id)">Accept</button>
+                                    <button class="bg-red-500 text-white rounded-lg p-2"
+                                        @click="refuse(companies.id)">Refuse</button>
+                                </div>
+                                <div v-else>
+                                    {{ companies.validation }}
+                                </div>
+
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
             </div>
         </section>
-
     </main>
-
 </template>
 <script setup>
 import { useRouter } from 'vue-router';
 import axios from 'axios';
-import { reactive , ref , onMounted} from 'vue';
-import { toast} from 'vue3-toastify';
+import { reactive, ref, onMounted } from 'vue';
+import { toast } from 'vue3-toastify';
 import { createToast } from 'mosha-vue-toastify';
-import 'mosha-vue-toastify/dist/style.css' 
+import 'mosha-vue-toastify/dist/style.css'
 
 const router = useRouter();
 
@@ -82,53 +82,57 @@ const test = ref('pending');
 
 const company = ref(null);
 const accept = (id) => {
-   axios.put('http://127.0.0.1:8000/api/accepted/'+id)
-    .then(response => {
-        createToast('the company get accepted',
-        {
-        
-        showIcon: 'true',
-        type: 'success',
-        position: 'bottom-left',
+    axios.put('http://127.0.0.1:8000/api/accepted/' + id)
+        .then(response => {
+            createToast('the company get accepted',
+                {
+
+                    showIcon: 'true',
+                    type: 'success',
+                    position: 'bottom-left',
+                })
+            //call the function to refresh the page
+            companies();
+
+
         })
-
-
-    })
-    .catch(error => {
-        console.log(error);
-    });
+        .catch(error => {
+            console.log(error);
+        });
 
 }
 const refuse = (id) => {
-    axios.put('http://127.0.0.1:8000/api/rejected/'+id)
-    .then(response => {
-        createToast('the company get accepted',
-        {
-        showIcon: 'true',
-        type: 'danger',
-        position: 'bottom-left',
+    axios.put('http://127.0.0.1:8000/api/rejected/' + id)
+        .then(response => {
+            createToast('the company get accepted',
+                {
+                    showIcon: 'true',
+                    type: 'danger',
+                    position: 'bottom-left',
+                })
+            //call the function to refresh the page
+            companies();
         })
-    })
-    .catch(error => {
-        console.log(error);
-    });
+        .catch(error => {
+            console.log(error);
+        });
 }
 //download document
 function download(url) {
     const extension = url.split('.').pop();
-      axios.get(url, { responseType: 'blob' })
+    axios.get(url, { responseType: 'blob' })
         .then(response => {
-          const blob = new Blob([response.data], { type: `application/${extension}` });
-          const url = window.URL.createObjectURL(blob);
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', `document.${extension}`);
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
+            const blob = new Blob([response.data], { type: `application/${extension}` });
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `document.${extension}`);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         })
         .catch(error => {
-          console.error(error);
+            console.error(error);
         });
 }
 const companies = async () => {
@@ -136,15 +140,15 @@ const companies = async () => {
         const response = await axios.get('http://127.0.0.1:8000/api/showCompany');
         company.value = response.data.data;
         var data = response.data.data;
-} catch (error) {
-    console.log(error);
-}
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-function check(status){
-    if(status == 'pending'){
+function check(status) {
+    if (status == 'pending') {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -153,7 +157,7 @@ onMounted(companies);
 
 
 
- //logout
+//logout
 
 const logout = () => {
     localStorage.removeItem('token')
