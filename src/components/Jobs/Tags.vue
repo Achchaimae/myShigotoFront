43304ae8ac592391b00c092dfbdd81775efe2064
achchaimae <achchaimae1@gmail.com>
2023-04-25 16:53:57 +0100

    tags done🥨

--- src/components/Jobs/Tags.vue
+++ src/components/Jobs/Tags.vue
@@ -1,33 +1,68 @@
 <template>
-    <div class="mt-8 flex justify-center">
-      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('UI')">
-        UX/UI
-      </button>
-      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('Web Developer')">
-        Web Developer
-      </button>
-      <button type="button" class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center" @click="searchPosts('Data Science')">
-        Data Science
-      </button>
-    </div>
-  </template>
-  
-  <script setup>
-    import axios from 'axios';
-  
-    const searchPosts = async (tag) => {
-      try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/V1/posts/tag/${tag}`);
-        const posts = response.data;
-       const data=posts[0];
-        console.log(data);
+  <div class="mt-8 flex justify-center">
+    <button type="button"
+      class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center"
+      @click="searchPosts('UI')">
+      UX/UI
+    </button>
+    <button type="button"
+      class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center"
+      @click="searchPosts('Web')">
+      Web Developer
+    </button>
+    <button type="button"
+      class="rounded-full px-4 mr-2 bg-[#9191E9] w-24 justify-center text-white p-2  leading-none flex items-center"
+      @click="searchPosts('Java')">
+      Java
+    </button>
+  </div>
 
-        
-        
-      } catch (error) {
-        // alert('Something went wrong')
-        console.error(error);
-      }
-    };
-  </script>
+  <h1 class="text-4xl font-bold px-4  w-full" v-if="data.length > 0">Searched By Tag</h1>
+  <div class="flex justify-around gap-4 flex-wrap text-white ">
+    <tagCard v-for="card in data" :Offreid="card.id" :CompanyName="card.title" :CompanyLogo="getImageUrl(card.image)"
+      :OffreName="card.title" :OffreDesciption="card.description" :createdAt="formatDate(card.created_at)" />
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { reactive, ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import tagCard from '@/components/Jobs/tagCard.vue'
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+const router = useRouter();
+//showing all the posts
+const posts = ref([]);
+const errors = ref([]);
+const data = ref([]);
+const searchPosts = async (tag) => {
+  try {
+    const response = await axios.get(`http://127.0.0.1:8000/api/V1/posts/tag/${tag}`);
+    data.value = response.data;
+    console.log(data.value);
+  } catch (error) {
+    // alert('Something went wrong')
+    console.error(error);
+  }
+};
+// to get how much hour ago the post was created
+const formatDate = (date) => {
+  return dayjs(date).fromNow();
+};
+
+// function to get the full URL of an image
+const getImageUrl = (imageName) => {
+  return `http://localhost:8000/storage/${imageName}`;
+};
+
+const obj = reactive({
+  cards: [],
+});
+
+
+</script>
   
---
+++ src/components/Jobs/tagCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="bg-white w-64 h-72 text-black rounded-lg mt-4">
+            <div class="flex">
+                <div class="relative">
+                    <img :src="'http://127.0.0.1:8000/' + CompanyLogo" class="absolute top-8 left-3 w-16" alt />
+                    <img src="@/assets/images/card.png" alt />
+                </div>
+                <div class="p-4">
+                    <h1 class="font-bold text-xl">{{ CompanyName }}</h1>
+                    <p>{{  createdAt }}</p>
+                </div>
+            </div>
+
+            <div class="p-4">
+                <h1 class="text-xl font-bold">{{ OffreName }}</h1>
+                <p class="font-regular text-gray-400 font-['Display_Fair']">{{ OffreDesciption }}</p>
+            </div>
+            <div class="flex justify-end px-4">
+                <button
+                    class="border-2 rounded-full px-8 py-2 mb-4 border-[#531CB3] hover:bg-[#531CB3] hover:text-white hover:border-white hover:cursor-pointer">
+                    
+                    <!-- <RouterLink to="/apply">Apply</RouterLink> -->
+                    <RouterLink :to="{name: 'Apply', params: {id: Offreid}}">
+                        Apply
+                    </RouterLink>
+
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+    Offreid: {
+        type: String,
+        required: true
+    },
+    CompanyLogo: {
+        type: String,
+        required: true
+    },
+    CompanyName: {
+        type: String,
+        required: true
+    },
+    OffreName: {
+        type: String,
+        required: true
+    },
+    OffreDesciption: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: String,
+        required: true
+    }
+});
+</script>
